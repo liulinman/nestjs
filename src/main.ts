ffb15455b4e73ced180d601a044bc78f589ee147
yifeng <2108546503@qq.com>
2025-03-04 14:40:41 +0800

    feat: init

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  console.log(`服务启动成功`);
+
   await app.listen(3000);
 }
 bootstrap();
