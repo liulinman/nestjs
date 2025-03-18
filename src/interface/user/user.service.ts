89f9d6d5f88267147d7e2074835a5287bc536340
yifeng <2108546503@qq.com>
2025-03-18 16:44:30 +0800

    feat:模型

--- src/interface/user/user.service.ts
+++ src/interface/user/user.service.ts
@@ -2,7 +2,7 @@
  * @Author: yifeng 2108546503@qq.com
  * @Date: 2025-03-14 13:38:58
  * @LastEditors: yifeng 2108546503@qq.com
- * @LastEditTime: 2025-03-17 11:39:17
+ * @LastEditTime: 2025-03-18 16:44:18
  * @FilePath: \bancked-project\src\interface\user\user.service.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,12 +11,12 @@ import { CreateUserDto, UpdateUserDto } from './dto/user.dto';
 import { initModels } from 'src/database/init-models';
 import { sequelizeModel } from 'src/config';
 
-const { user } = initModels(sequelizeModel);
+const { users } = initModels(sequelizeModel);
 
 @Injectable()
 export class UserService {
   async create(createUserDto: CreateUserDto) {
-    return await user.create(createUserDto);
+    return await users.create(createUserDto);
   }
 
   async findAll() {
