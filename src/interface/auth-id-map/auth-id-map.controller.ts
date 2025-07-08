61794412ca8c803ff54b4052db41b64bfca5c3b3
yifeng <2108546503@qq.com>
2025-07-08 14:47:08 +0800

    feat: 添加新接口-待完善

--- src/interface/auth-id-map/auth-id-map.controller.ts
+++ src/interface/auth-id-map/auth-id-map.controller.ts
@@ -14,16 +14,53 @@ import { UpdateAuthIdMapDto } from './dto/update-auth-id-map.dto';
 export class AuthIdMapController {
   constructor(private readonly authIdMapService: AuthIdMapService) {}
 
+  /**新增修改cookie */
   @Post('/createCookie')
   create(@Body() createAuthIdMapDto: any) {
     return this.authIdMapService.create(createAuthIdMapDto);
   }
 
+  /**查询erp模块权限 */
   @Post('/findModel')
   findAll(@Body() data: any) {
     return this.authIdMapService.findAll(data);
   }
 
+  /**查找汉字是否存在 */
+  @Post('/findWorld')
+  findWorld(@Body() data: any) {
+    return this.authIdMapService.findAll(data);
+  }
+
+  /**创建或者修改单词 */
+  @Post('/createWorld')
+  createWorld(@Body() data: any) {
+    return this.authIdMapService.findAll(data);
+  }
+
+  /**删除单词-直接删除 */
+  @Post('/deleteWorld')
+  deleteWorld(@Body() data: any) {
+    return this.authIdMapService.findAll(data);
+  }
+
+  /**查找所有单词 */
+  @Post('/findAllWorld')
+  findAllWorld(@Body() data: any) {
+    return this.authIdMapService.findAll(data);
+  }
+
+  /**查找某个单词-模糊查询 */
+  @Post('/findOneWorld')
+  findOneWorld(@Body() data: any) {
+    return this.authIdMapService.findAll(data);
+  }
+
+  /**映射英语单词 */
+  @Post('/mappingWorld')
+  mappingWorld(@Body() data: any) {
+    return this.authIdMapService.findAll(data);
+  }
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.authIdMapService.findOne(+id);
