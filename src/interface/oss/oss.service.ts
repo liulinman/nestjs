b4057ad33933ce75297e3b460441f4f91a49000f
yifeng <2108546503@qq.com>
2025-09-17 17:29:43 +0800

    fix: 分页

--- src/interface/oss/oss.service.ts
+++ src/interface/oss/oss.service.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: yifeng 2108546503@qq.com
+ * @Date: 2025-08-31 21:08:19
+ * @LastEditors: yifeng 2108546503@qq.com
+ * @LastEditTime: 2025-09-17 17:21:50
+ * @FilePath: /nestjs/src/interface/oss/oss.service.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { Injectable } from '@nestjs/common';
 import * as OSS from 'ali-oss';
 
@@ -7,12 +15,7 @@ export class OssService {
 
   constructor() {
     // 初始化阿里云 OSS 客户端
-    this.client = new OSS({
-      region: 'oss-cn-chengdu', // 填写你的OSS区域
-      accessKeyId: 'LTAI5t7fUwUTbMjUGkaWExad', // 填写你的Access Key ID
-      accessKeySecret: 'BKvlAfBsYfIWAe74Q33AmSLKHoNsu6', // 填写你的Access Key Secret
-      bucket: 'englishpicture', // 填写你的Bucket名称
-    });
+    this.client = new OSS({});
   }
 
   // 上传图片
